98f408c02153264859e24da98ff222866eda54bf
Havid <great_havid@yahoo.co.id>
2019-11-11 14:18:57 +0700

    Tugas 3 JS

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+// ====== Tugas 3 JavaScript =====
+
+function returnBack() {
+    let message = "Bootcamp";
+    message = "Niomic";
+  
+    return message;
+  }
+  
+  console.log(returnBack());
+  
